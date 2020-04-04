293cb27b77ed204f7b1ad0513bcca197d8fef487
Vladimir J <vladdaimpaler@protonmail.com>
2020-04-04 19:32:15 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,7 +1,7 @@
 def prime?(input)
   isitprime=false
   if input.to_i > 1
-    [2..input].each{|n| input % n == 0 ? isitprime = true : isitprime = false}
+    [2..input.to_i].each{|n| input % n == 0 ? isitprime = true : isitprime = false}
   end
   return isitprime
 end
